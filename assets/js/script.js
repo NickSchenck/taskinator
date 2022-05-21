725ce7c6b8051c60c432db2655afd240e2211e80
NickSchenck <yoyo4games@yahoo.com>
2022-05-21 15:36:21 -0500

    learning form submitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //This line links the var with the id #tasks-to-do
 
-var createTaskHandler = function(){
+var createTaskHandler = function(event) { //event is a way to target something happening on the page
+  event.preventDefault(); //preventDefault in this case prevents the submit event from reloading the page automatically
+    
     var listItemEl = document.createElement("li"); // this creates an li element with the name listItemEl
     listItemEl.className = "task-item"; //This line targets/assigns the class task-item to the var, in this case styling the created li
     listItemEl.textContent = "This is a new task."; //This line determines what text the created li should contain
     tasksToDoEl.appendChild(listItemEl); //this line makes new li a child of a different var
 };
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler); //submit listens for a click event of a button with the type attribute of submit, or Enter being pressed on the keyboard 
